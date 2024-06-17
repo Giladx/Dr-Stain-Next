8f3204935f31742854e33edb50ba1eb5fe24a31a
Gilad Levi <gilad.x@gmail.com>
2024-06-17 12:00:21 +0300

    Commit from TeleportHQ

--- pages/_document.js
+++ pages/_document.js
@@ -61,7 +61,7 @@ class CustomDocument extends Document {
           <noscript
             dangerouslySetInnerHTML={{
               __html:
-                '</noscript><style>\nhtml {\nscroll-behavior: smooth;\n}\n</style><noscript>',
+                "</noscript><style>\nhtml {\nscroll-behavior: smooth;\n}\n</style>\n\n<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-PLNTMT38');</script>\n<!-- End Google Tag Manager --><noscript>",
             }}
           ></noscript>
         </Head>
@@ -71,7 +71,7 @@ class CustomDocument extends Document {
           <div
             dangerouslySetInnerHTML={{
               __html:
-                '<script defer src=\'https://unpkg.com/@teleporthq/teleport-custom-scripts\'></script>\n<!-- Google Tag Manager (noscript) -->\n<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLNTMT38"\nheight="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>\n<!-- End Google Tag Manager (noscript) -->\n        <script defer src=\'https://unpkg.com/@teleporthq/react-components/dist/animate-on-reveal.umd.js\'></script>',
+                '<script defer src=\'https://unpkg.com/@teleporthq/teleport-custom-scripts\'></script>\n\n<!-- Google Tag Manager (noscript) -->\n<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLNTMT38"\nheight="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>\n<!-- End Google Tag Manager (noscript) -->\n        <script defer src=\'https://unpkg.com/@teleporthq/react-components/dist/animate-on-reveal.umd.js\'></script>',
             }}
           ></div>
         </body>
