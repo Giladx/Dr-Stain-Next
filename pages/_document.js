0896daaa4794c468a5e95046ce19ec55d2d7f34a
Gilad Levi <gilad.x@gmail.com>
2024-06-17 13:22:53 +0300

    Commit from TeleportHQ

--- pages/_document.js
+++ pages/_document.js
@@ -61,7 +61,7 @@ class CustomDocument extends Document {
           <noscript
             dangerouslySetInnerHTML={{
               __html:
-                "</noscript><style>\nhtml {\nscroll-behavior: smooth;\n}\n</style>\n\n<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-PLNTMT38');</script>\n<!-- End Google Tag Manager --><noscript>",
+                '</noscript><style>\nhtml {\nscroll-behavior: smooth;\n}\n</style><noscript>',
             }}
           ></noscript>
         </Head>
